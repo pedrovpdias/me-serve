d01d9511a9dbdc43a1ae5264ad8fe558362135db
Pedro Dias <pedrovpdias@gmail.com>
2025-05-06 22:24:30 -0300

    Install Sanctum

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+
+axios.defaults.withCredentials = true; // Enable sending cookies with cross-origin requests 
+axios.defaults.withXSRFToken = true; // Enable sending CSRF token with cross-origin requests
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
