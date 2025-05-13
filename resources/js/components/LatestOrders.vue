87f103a938ff5e5a8b8edb91717165d1a2a241ed
Pedro Dias <pedrovpdias@gmail.com>
2025-05-12 23:36:11 -0300

    Update LatestOrders

--- resources/js/components/LatestOrders.vue
+++ resources/js/components/LatestOrders.vue
@@ -20,24 +20,24 @@
   <div class="flex flex-col gap-4 w-fit text-sm flex-1">
     <Subtitle :text="'Pedidos recentes'" />
 
-    <table class="w-fit flex-none border-separate border-spacing-2">
-      <thead>
+    <table class="w-full flex-none border border-primary/10">
+      <thead class="border-b border-primary/10">
         <tr>
-          <th class="text-left">Nº do pedido</th>
-          <th class="text-left">Valor</th>
-          <th class="text-left">Status</th>
+          <th class="text-left p-2">Nº do pedido</th>
+          <th class="text-left p-2">Valor</th>
+          <th class="text-left p-2">Status</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="order in orders" :key="order.id">
-          <td class="text-left font-semibold">
+        <tr v-for="order in orders" :key="order.id" class="odd:bg-secondary hover:bg-primary/10">
+          <td class="text-left font-semibold p-2">
             #{{ order.id }}
           </td>
-          <td class="text-left">
+          <td class="text-left p-2">
             R$ {{ order.total }}
           </td>
-          <td class="text-left">
+          <td class="text-left p-2">
             {{ order.description }}
           </td>
         </tr>
@@ -45,7 +45,9 @@
     </table>
 
     <div class="flex justify-end">
-      <router-link to="/pedidos" class="bg-secondary w-full text-sm font-semibold py-1 px-2 text-center">Ver todos</router-link>
+      <router-link to="/pedidos" class="bg-secondary w-full text-sm font-semibold py-1 px-2 text-center hover:bg-primary/10 focus:bg-primary/10 active:shadow-inner outline-none">
+        Ver todos
+      </router-link>
     </div>
   </div>
 </template>
