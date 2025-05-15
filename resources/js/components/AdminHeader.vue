b3b7bbc4d07ebf7ede30d58a1aa65b26116f45a8
Pedro Dias <pedrovpdias@gmail.com>
2025-05-15 12:46:37 -0300

    Update AdminHeader

--- resources/js/components/AdminHeader.vue
+++ resources/js/components/AdminHeader.vue
@@ -17,20 +17,22 @@
 
 <template>
   <header class="w-full flex justify-between items-center px-8 pb-4 border-b border-b-primary/10">
-      <img :src="logo" alt="Logo" class="w-16 h-auto">
+      <router-link to="/admin">
+        <img :src="logo" alt="Logo" class="w-16 h-auto">
+      </router-link>
 
       <button class="flex gap-4 items-center text-red-600 font-semibold relative group hover:text-red-800 focus:text-red-800 outline-none" @click="toggleLogoutDropdown()">
         {{ user.name }}
 
-        <span class="size-8 aspect-square grid place-content-center rounded-full bg-red-600 group-hover:bg-red-800 group-focus:bg-red-800 outline-none text-white">
+        <span class="size-8 aspect-square grid place-content-center rounded-full bg-red-600 group-hover:bg-red-800 group-focus:bg-red-800 outline-none text-white pointer-events-none">
           <i class="bi bi-person-fill"></i>
         </span>
 
-        <div v-if="showLogoutDropdown" class="absolute top-12 right-0 bg-white shadow rounded-lg px-4 py-2 flex flex-col gap-2 text-sm">
-          <button @click="auth.logout()" class="text-red-600 font-semibold flex items-center gap-2 hover:text-red-800 focus:text-red-800 outline-none">
+        <div v-if="showLogoutDropdown" class="absolute top-12 right-0 bg-white shadow rounded-lg px-4 py-2 flex flex-col gap-2 text-sm pointer-events-none">
+          <button @click="auth.logout()" class="text-red-600 font-semibold flex items-center gap-2 hover:text-red-800 focus:text-red-800 outline-none pointer-events-none">
             Sair
 
-            <i class="bi bi-box-arrow-right"></i>
+            <i class="bi bi-box-arrow-right pointer-events-none"></i>
           </button>
         </div>
       </button>
