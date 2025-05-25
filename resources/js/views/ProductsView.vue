edce8b66d2fa084568d94befcc62635992bba98a
Pedro Dias <pedrovpdias@gmail.com>
2025-05-24 22:55:21 -0300

    Add Novo produto link

--- resources/js/views/ProductsView.vue
+++ resources/js/views/ProductsView.vue
@@ -2,6 +2,7 @@
   import AdminHeader from '../components/AdminHeader.vue'; // Importa o AdminHeader
   import Title from '../components/Title.vue'; // Importa o título
   import ProductsTable from '../components/ProductsTable.vue'; // Importa a tabela de produtos
+import router from '../router';
 
   const breadcrumbLinks = [
     {
@@ -22,6 +23,16 @@
 
     <div class="flex flex-col gap-8 px-8">
       <Title :text="'Produtos'" />
+
+      <router-link to="/admin/products/new" class="flex gap-2 items-center font-bold self-end text-red-600 hover:text-red-800 focus:text-red-800 group outline-none">
+        <span class="text-sm">
+          <i class="bi bi-plus-circle"></i>
+        </span>
+
+        <span class="group-hover:underline group-focus:underline">
+          Novo produto
+        </span>
+      </router-link>
 
       <ProductsTable />
     </div>
